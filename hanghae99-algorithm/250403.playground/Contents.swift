eeb311a40241fb5aa3b02bac2a92ec17208e4dc8
kimhyeji-pluscompany <kimhyeji@pluscompany.biz>
2025-04-03 10:59:27 +0900

    [#6] 주석 추가

--- hanghae99-algorithm/250403.playground/Contents.swift
+++ hanghae99-algorithm/250403.playground/Contents.swift
@@ -19,6 +19,11 @@ class MyQueue {
     func peek() -> Int {
         guard output.isEmpty else { return output.last! }
         return input.first!
+        /*
+         guard output.isEmpty else { return output[output.endIndex - 1] }
+         return input[input.startIndex]
+         두 코드는 기능과 동작이 모두 동일하지만, 코드표현력 측면에서 last,first와 같이 명시적인 프로퍼티 사용을 최종 최적화로 생각했습니다.
+         */
     }
     
     func empty() -> Bool {
