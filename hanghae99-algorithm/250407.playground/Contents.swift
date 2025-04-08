f8cf896755da1c11c5d51fdc15a86e579e203322
kimhyeji-pluscompany <kimhyeji@pluscompany.biz>
2025-04-08 16:55:04 +0900

    [#17] TLE 발생으로 인해 코드 개선

--- hanghae99-algorithm/250407.playground/Contents.swift
+++ hanghae99-algorithm/250407.playground/Contents.swift
@@ -1,10 +1,16 @@
 class Solution {
     func climbStairs(_ n: Int) -> Int {
-        switch n {
-            case 0: return 0
-            case 1: return 1
-            case 2: return 2
-            default: return climbStairs(n-1) + climbStairs(n-2)
+        guard n > 1 else { return 1 }
+        
+        // index를 계단 단계로 가정
+        var dp = Array(repeating: 0, count: n+1)
+        dp[0] = 1   // 0번째 계단까지의 경우의 수
+        dp[1] = 1   // 1번째 계단까지의 경우의 수
+        for i in 2...n {
+            dp[i] = dp[i - 1] + dp[i - 2]
         }
+        return dp[n]
     }
 }
+
+print(Solution().climbStairs(45))
