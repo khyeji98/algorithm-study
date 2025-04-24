92ed96fa18528594765cc279ab77d8636694f2fe
khyeji98 <gpwl981207@naver.com>
2025-04-24 20:05:36 +0900

    [#28] 이분탐색 풀이로 개선

--- hanghae99-algorithm/250422.playground/Contents.swift
+++ hanghae99-algorithm/250422.playground/Contents.swift
@@ -1,18 +1,37 @@
 class Solution {
     func findTheDistanceValue(_ arr1: [Int], _ arr2: [Int], _ d: Int) -> Int {
-        var count = 0
+        let sortedArr2 = arr2.sorted()
+        var counts = 0
         for a1 in arr1 {
-            guard meetTheCondition(a1, arr2, d) else { continue }
-            count += 1
+            let index = sortedArr2.binarySearchInsertPosition(of: a1)
+            if index < sortedArr2.count, abs(sortedArr2[index]-a1) <= d {
+                continue
+            }
+            if index > 0, abs(sortedArr2[index-1]-a1) <= d {
+                continue
+            }
+            counts += 1
         }
-        return count
-    }
-    
-    private func meetTheCondition(_ a1: Int, _ arr2: [Int], _ d: Int) -> Bool {
-        for a2 in arr2 {
-            guard abs(a1-a2) <= d else { continue }
-            return false
-        }
-        return true
+        return counts
     }
 }
+
+extension Array where Element == Int {
+    func binarySearchInsertPosition(of target: Int) -> Int {
+        var low = 0, high = count
+        // 점점 좁혀가며 정렬상 target이 있어야 할 위치 찾기
+        while low < high {
+            let mid = (low + high) / 2
+            if self[mid] < target {
+                // 시작점 변경
+                low = mid + 1
+            } else {
+                // 끝점 변경
+                high = mid
+            }
+        }
+        return low
+    }
+}
+
+Solution().findTheDistanceValue([4,5,8], [10,9,1,8], 2)
