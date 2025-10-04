72c4fdf20e018194ac0ed4b668303c930bb5e815
khyeji98 <gpwl981207@naver.com>
2025-10-04 17:33:27 +0900

    [#35] 1차 풀이 코드 개선 - 수식 정리 - 불필요한 케이스 제거

--- python/35. Search Insert Position.py
+++ python/35. Search Insert Position.py
@@ -5,7 +5,7 @@ class Solution:
         left, right = 0, len(nums) - 1
 
         while left <= right:
-            mid = left + (right - left) // 2
+            mid = (left + right) // 2
 
             if nums[mid] == target:
                 return mid
@@ -13,8 +13,5 @@ class Solution:
                 left = mid + 1
             else:
                 right = mid - 1
-
-        if nums[left] < target:
-            return left + 1
-        else:
-            return left
+                
+        return left
