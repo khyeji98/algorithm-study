2deb71ba7034c8bb17308aa069d823a7ff4b9bd3
khyeji98 <gpwl981207@naver.com>
2025-10-19 16:32:03 +0900

    [#46] 리팩토링 및 복잡도 주석 추가

--- python/100. Same Tree.py
+++ python/100. Same Tree.py
@@ -6,12 +6,12 @@ class TreeNode:
         self.left = left
         self.right = right
 
+# Time Complexity: O(n)
+# Space Complexity: O(h) -> h는 트리의 높이
 class Solution:
     def isSameTree(self, p: Optional[TreeNode], q: Optional[TreeNode]) -> bool:
         if not p and not q: return True # p와 q 둘 다 None인 경우
         if not p or not q: return False # p와 q 중 하나만 None인 경우
 
-        if p.val != q.val: return False
-        if self.isSameTree(p.left, q.left) == False: return False
-        if self.isSameTree(p.right, q.right) == False: return False
-        return True
+        if p.val != q.val: return False # 노드의 값이 다른 경우
+        return self.isSameTree(p.left, q.left) and self.isSameTree(p.right, q.right) # 왼쪽과 오른쪽 서브트리 비교
