00b3aa36904bac311a4cda8a18e2195044effa8a
khyeji98 <gpwl98wkd@gmail.com>
2025-10-29 11:19:57 +0900

    [#50] 코드 개선

--- python/20. Valid Parentheses.py
+++ python/20. Valid Parentheses.py
@@ -1,17 +1,20 @@
 # Time Complexity: O(n) -> n은 문자열 s의 길이
 # Space Complexity: O(n) -> 최악의 경우 모든 여는 괄호가 스택에 쌓일 수 있음
+
 class Solution:
     def isValid(self, s: str) -> bool:
-        matching = {')': '(', '}': '{', ']': '['}
+        if len(s) & 1: return False # 정수의 마지막 비트가 1이면 홀수라는 뜻, 문자열 길이가 홀수면 무조건 False
+
+        pair = {')': '(', '}': '{', ']': '['}
         stack = []
+        push = stack.append
+        pop = stack.pop # 파이썬의 리스트는 pop을 제공함
 
         for char in s:
-            if char in '({[':
-                stack.append(char)
+            if char in pair.values():  # 여는 괄호일 경우
+                push(char)
             else:
-                if not stack: return False  # 스택이 비어있는데 pop()을 호출하면 오류 발생하기 때문에 먼저 확인
-                last = stack.pop()
-                matched = matching[char]
-                if last != matched: return False
+                # 스택이 비어있는데 pop()을 호출하면 오류 발생하기 때문에 먼저 확인
+                if not stack or pop() != pair[char]: return False
 
         return len(stack) == 0
