134fde2042bd6130820550509f1445679953ef4b
khyeji98 <gpwl98wkd@gmail.com>
2026-01-07 11:48:58 +0900

    [#56] 1차 풀이 개선

--- swift/3190-Find-Minimum-Operations-to-Make-All-Elements-Divisible-by-Three.swift
+++ swift/3190-Find-Minimum-Operations-to-Make-All-Elements-Divisible-by-Three.swift
@@ -1,15 +1,15 @@
+// Time Complexity: O(n)
+// Space Complexity: O(1)
 class Solution {
     func minimumOperations(_ nums: [Int]) -> Int {
         var result: Int = 0
 
         for num in nums {
-            guard num > 0 else { continue }
-            let charge = num % 3
-            let remainToThree = abs(3 - charge)
-            if charge > remainToThree {
-                result += remainToThree
-            } else {
-                result += charge
+            let mod = num % 3   // mod = modulo(나머지)
+            // 3으로 나눈 나머지가 0이 아니면, 1을 더하거나 빼는 방법 밖에 없음
+            // 따라서 3으로 나눈 나머지가 1 또는 2인 경우 모두 1번의 연산이 필요 (= 값의 차를 구할 필요 없음)
+            if mod != 0 {
+                result += 1
             }
         }
 
