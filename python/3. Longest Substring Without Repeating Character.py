1e2e71ee3d71d49fb34e3e2e26897fe04ada145f
khyeji98 <gpwl98wkd@gmail.com>
2026-01-13 13:40:40 +0900

    [#61] 파이썬 코드 풀이

---
+++ python/3. Longest Substring Without Repeating Character.py
@@ -0,0 +1,16 @@
+# https://leetcode.com/problems/longest-substring-without-repeating-characters
+
+class Solution:
+    def lengthOfLongestSubstring(self, s: str) -> int:
+        max_length = 0
+        leading = 0
+        char_index = {} # <= table
+
+        for (trailing, char) in enumerate(s):
+            if char in char_index and char_index[char] >= leading:
+                leading = char_index[char] + 1
+            
+            char_index[char] = trailing
+            max_length = max(max_length, trailing - leading + 1)
+        
+        return max_length
